792d6a9cf997cd894c88e3e6acd35af05d577130
elharony <p.yahyaelharony@gmail.com>
2018-03-22 01:27:54 +0200

    Write the functionality and content of the `typed.js` container

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
 })
+
+$(function(){
+	$("#typed").typed({
+		strings: ["Manage real estate units", "Lorem ipsum dolor sit.", "Sed eu tellus feugiat."],
+		// Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+		stringsElement: null,
+		// typing speed
+		typeSpeed: 30,
+		// time before typing starts
+		startDelay: 1200,
+		// backspacing speed
+		backSpeed: 20,
+		// time before backspacing
+		backDelay: 500,
+		// loop
+		loop: true
+	});
+});
